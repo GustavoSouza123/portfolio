c73ce883c67e44168a071831174567f2d0a3f7d6
Gustavo Souza <gustavoelia7@gmail.com>
2023-11-18 17:31:44 -0300

    feats: config files, assets folder; fix: mobile nav styles

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require 'config/config.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,23 +12,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/style.css" /> <!-- css file -->
+        <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/style.css" /> <!-- css file -->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script> <!-- jQuery API -->
     <title>Portfólio | Gustavo Souza</title>
 </head>
 <body>
     <header>
         <div class="social">
-            <a href="https://github.com/GustavoSouza123" target="_blank"><img src="images/github.svg" alt="Github logo" /></a>
-            <a href="https://www.linkedin.com/in/gustavo-souza-316003272/" target="_blank"><img src="images/linkedin.svg" alt="LinkedIn logo" /></a>
-            <a href="https://twitter.com/gustavosouza456" target="_blank"><img src="images/twitter.svg" alt="Twitter logo" /></a>
+            <a href="https://github.com/GustavoSouza123" target="_blank"><img src="<?php echo INCLUDE_PATH; ?>assets/images/github.svg" alt="Github logo" /></a>
+            <a href="https://www.linkedin.com/in/gustavo-souza-316003272/" target="_blank"><img src="<?php echo INCLUDE_PATH; ?>assets/images/linkedin.svg" alt="LinkedIn logo" /></a>
+            <a href="https://twitter.com/gustavosouza456" target="_blank"><img src="<?php echo INCLUDE_PATH; ?>assets/images/twitter.svg" alt="Twitter logo" /></a>
         </div>
         <nav>
             <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Portfolio</a></li>
-                <li><a href="">Blog</a></li>
+                <li><a href="blog/">Blog</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
         </nav> 
@@ -41,6 +43,6 @@
 
     <footer></footer>
 
-    <script src="js/script.js"></script> <!-- main js file -->
+    <script src="<?php echo INCLUDE_PATH; ?>assets/js/script.js"></script> <!-- main js file -->
 </body>
 </html>
