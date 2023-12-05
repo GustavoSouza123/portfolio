6fd95befb591a1e235848db338df5556e3143666
Gustavo Souza <gustavoelia7@gmail.com>
2023-12-05 15:39:54 -0300

    fix: page loading spinner bug

--- index.php
+++ index.php
@@ -31,10 +31,6 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script> <!-- jQuery API -->
     <script>
         // page loading
-        $(document).ready(function() {
-            $('.loading').css('display', 'flex').hide().fadeIn(0);
-        })
-
         $(window).on('load', function() {
             $('.loading').fadeOut(200);
         })
