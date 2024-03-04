f0619249b227721d0c9b58bd2914e0a31eb4dad6
Gustavo Souza <gustavoelia7@gmail.com>
2024-03-04 12:18:07 -0300

    fix: error message if there is no featured blog posts

--- index.php
+++ index.php
@@ -198,24 +198,28 @@
                  <?php
                     $sql = $pdo->prepare("SELECT * FROM `tb_posts` WHERE published = 1 AND featured = 1");
                     $sql->execute();
-                    $posts = $sql->fetchAll(PDO::FETCH_ASSOC);
-                    foreach($posts as $key => $value) {
-                        $sql = $pdo->prepare("SELECT `name` FROM `tb_categories` WHERE id = ?");
-                        $sql->execute(array($value['category_id']));
-                        $category = $sql->fetchColumn();
-                        echo '
-                        <div class="post">
-                            <div class="image">
-                                <img loading="lazy" src="'.INCLUDE_PATH_BLOG.'admin/'.$value['thumbnail'].'" alt="" />
+                    if($sql->rowCount() == 0) {
+                        echo '<div class="no-post">Nenhum post encontrado</div>';
+                    } else {
+                        $posts = $sql->fetchAll(PDO::FETCH_ASSOC);
+                        foreach($posts as $key => $value) {
+                            $sql = $pdo->prepare("SELECT `name` FROM `tb_categories` WHERE id = ?");
+                            $sql->execute(array($value['category_id']));
+                            $category = $sql->fetchColumn();
+                            echo '
+                            <div class="post">
+                                <div class="image">
+                                    <img loading="lazy" src="'.INCLUDE_PATH_BLOG.'admin/'.$value['thumbnail'].'" alt="" />
+                                </div>
+                                <div class="content">
+                                    <div class="title">'.$value['title'].'</div>
+                                    <div class="subtitle">'.$value['subtitle'].'</div>
+                                    <div class="read-more"><a href="'.INCLUDE_PATH_BLOG.'article?id='.$value['id'].'">Ler mais</a></div>
+                                    <div class="category">'.$category.'</div>
+                                </div>
                             </div>
-                            <div class="content">
-                                <div class="title">'.$value['title'].'</div>
-                                <div class="subtitle">'.$value['subtitle'].'</div>
-                                <div class="read-more"><a href="'.INCLUDE_PATH_BLOG.'article?id='.$value['id'].'">Ler mais</a></div>
-                                <div class="category">'.$category.'</div>
-                            </div>
-                        </div>
-                        ';
+                            ';
+                        }
                     }
                 ?>
                 </div>
